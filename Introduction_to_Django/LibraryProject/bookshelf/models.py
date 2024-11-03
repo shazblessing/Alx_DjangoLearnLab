1e9c835ca54fab2fc65bfce91252beab209a682b
Wanjiku <wanjikusharon59@gmail.com>
2024-11-03 23:57:21 +0300

    server

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -6,6 +6,6 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
    
-    def __str__ (self):
-        return self.title
+    #def __str__ (self):
+        #return self.title
     
