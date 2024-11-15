28be649d5682156693e57a045ade8db40149c80c
Wanjiku <wanjikusharon59@gmail.com>
2024-11-15 19:55:18 +0300

    amended

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,9 +1,3 @@
-from django.shortcuts import render
-from .models import Book
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, "relationship_app/list_books.html", {"books":books})
-
 from .forms import BookForm
 from django.views.generic import DetailView
 from django.views.generic.detail import DetailView
