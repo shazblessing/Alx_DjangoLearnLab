941e1ef067b9e07540ebb13532f649249a38d2d6
Wanjiku <wanjikusharon59@gmail.com>
2024-11-17 18:40:36 +0300

    amend

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -14,3 +14,15 @@ def list_books_in_library(library_name):
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
     return Librarian.objects.get(library=library)
+
+
+from .models import Author, Library
+
+
+objects.filter(author=author)
+Author.objects.get(name=author_name)
+
+books = Library.objects.all()
+Librarian.objects.get(library=)
+Library.objects.get(name=library_name)
+books.all()
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,9 +4,12 @@ from django.shortcuts import render,get_object_or_404
 from django.views.generic import DetailView
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
+from django.views.generic.detail import DetailView  # Correctly add import here
+from .models import Library
+
 
 
 def get_books(request): 
@@ -27,22 +30,7 @@ class BookDetail(DetailView):
         library= get_object_or_404(Book,book=self.kwargs.get("book"))
         return Library.objects.get(library=library)
     
-    def user_login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('home')  # Replace 'home' with your preferred route
-    else:
-        form = AuthenticationForm()
-    return render(request, 'relationship_app/login.html', {'form': form})
-
-def user_logout(request):
-    logout(request)
-    return render(request, 'relationship_app/logout.html')
-
-def user_register(request):
+    def user_register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -51,3 +39,23 @@ def user_register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Change 'home' to an appropriate view or route
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
