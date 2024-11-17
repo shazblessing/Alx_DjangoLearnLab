f5aea6a0da494a04f1e29df9e93e851ab7c37b70
Wanjiku <wanjikusharon59@gmail.com>
2024-11-17 15:08:33 +0300

    amended

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,31 +1,18 @@
-from .forms import BookForm
-from django.views.generic import DetailView
+ # relationship_app/views.py
+from django.shortcuts import render
+from .models import Book
+
+def list_books(request):
+    books = Book.objects.all()  # Query all book entries
+    context = {'books': books}
+    return render(request, 'relationship_app/list_books.html', context)
+
+
 from django.views.generic.detail import DetailView
-from .models import Library  # Adjust the model import as needed
+from .models import Library
 
 class LibraryDetailView(DetailView):
+    # Define the model and template to use
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-   from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
-from django.contrib.auth.views import LoginView, LogoutView
-
-class CustomLoginView(LoginView):
-    template_name = 'relationship_app/login.html'
-
-class CustomLogoutView(LogoutView):
-    template_name = 'relationship_app/logout.html'
-
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home')  # Redirect to a home page or any appropriate view
-    else:
-        form = UserCreationForm()
-    return render(request, 'relationship_app/register.html', {'form': form})
- 
