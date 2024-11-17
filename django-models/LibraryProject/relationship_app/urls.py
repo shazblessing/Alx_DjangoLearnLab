e1c4313ffe087011fea86ecb648cb625816460d8
Wanjiku <wanjikusharon59@gmail.com>
2024-11-17 18:19:29 +0300

    ammended

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,17 +1,23 @@
-from django import path 
-
-from .import views 
-urlpatterns=[
-    path("books/",views.get_books,name="get_books"), 
-    path("book-detail/<int:pk>/",views.BookDetail.as_view(),name="book_detail")
-]
-
+from .models import Book, Library
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView
-
+from .views import LibraryDetailView
+from .views import list_books
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', views.user_register, name='register'),
+    path('book/', views.viewmodel,name = 'bookd' ),
+    path('library/', LibraryDetailView.as_view(), name = 'list' ),
+    path('register/', views.register),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+   
+
+    path('profile/', views.ProfileView.as_view(), name = 'profile'),
+    path('admin/', views.admin_view, name = 'admin_view'),
+    path('librarian/', views.librarian_view, name = 'librarian_view'),
+    path('member/', views.member_view, name = 'member_view'),
+    path('add_book/', views.add_book, name = 'add_book'),
+    path('edit_book/', views.edit_book, name = 'edit_book'),
+    path('delete_book/', views.delete_book, name='delete_book'),
 ]
