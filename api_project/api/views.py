e4b507a8e20a61c62dcca78dac491bd6ae25f3f5
Wanjiku <wanjikusharon59@gmail.com>
2024-11-29 14:20:47 +0300

    crud

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -10,4 +10,8 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer    
