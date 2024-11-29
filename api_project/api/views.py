fc057c2453283381913f82b7a415e12201f57cd7
Wanjiku <wanjikusharon59@gmail.com>
2024-11-29 14:15:41 +0300

    SERIALIZER

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import generics
 from .models import Book
-from serializers import BookSerializer
+from .serializers import BookSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    
