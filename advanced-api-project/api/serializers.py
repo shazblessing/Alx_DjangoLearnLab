d5eb30f5bbffccb1ed6851e88400a590feb33961
Wanjiku <wanjikusharon59@gmail.com>
2024-12-01 22:30:44 +0300

    amended

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -6,6 +6,11 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields= '__all__'
 
+    def validate_publication_year(self, value):  # Custom validation to ensure publication year is not in the future
+        if value > 2024:
+            raise serializers.ValidationError('publication can not be in the future')
+        return value
+
 class AuthorSerializer(serializers.ModelSerializer):
     books= BookSerializer(many=True, read_only=True) # NESTED SERIALIZER TO SHOW RELATED BOOKS
     class Meta: 
