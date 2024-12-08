cdab296e26ed11109e77a9e022057f41fcde10f7
Wanjiku <wanjikusharon59@gmail.com>
2024-12-08 16:13:36 +0300

    ammends

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -45,3 +45,33 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+    
+
+
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('post_list')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        # Handle profile update here (e.g., email change)
+        user = request.user
+        user.email = request.POST.get('email')
+        user.save()
+        return redirect('profile')
+    return render(request, 'blog/profile.html', {'user': request.user})
