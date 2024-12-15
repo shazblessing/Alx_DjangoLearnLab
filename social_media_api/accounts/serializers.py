486d5c03a1ee5ec5c2d8eeedd0bd5e552ca817b7
Wanjiku <wanjikusharon59@gmail.com>
2024-12-15 21:10:44 +0300

    ammends

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
-from .models import CustomUser
-from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
 
-class UserSerializer(serializers.ModelSerializer):
+User = get_user_model()
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
     class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+
+    def create(self, validated_data):
+        # Create a new user
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        # Create a token for the new user
+        Token.objects.create(user=user)
+        return user
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -57,3 +57,23 @@ class LoginView(APIView):
             token, _ = Token.objects.get_or_create(user=user)
             return Response({'token': token.key}, status=status.HTTP_200_OK)
         return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from .serializers import UserLoginSerializer
+
+class UserLoginView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                "token": token.key,
+                "username": user.username,
+                "email": user.email
+            })
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
