bb7d47aa79b32b0b458138988a720c6d493d53e1
Wanjiku <wanjikusharon59@gmail.com>
2024-12-15 20:32:19 +0300

    changes

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
     'rest_framework',
     'rest_framework.authtoken',
-    'accounts',
+    
 ]
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
